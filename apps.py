d4da95105ded16a667c88c70afa681b7f150def0
Sebastian-byte <sebastianmontoya209@gmail.com>
2020-11-15 02:52:34 -0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -3,7 +3,7 @@ import subprocess
 import sys
 from random import randint
 
-all_commands = {"hello": "A simple command that prints 'Hello World!'", "numguess": "An awesome Guess-The-Number Game", "ls": "Lists all the files and directories inside the home folder", "new": "Creates a new file, use with 'new [filename]', i.e 'new hi.txt', do never use whitespace.", "show": "Prints the contents of a file", "help": "Shows information about a available commands", "exit": "Stops the execution of FakeOS", "pyshell": "Opens an embedded python3 shell", "pyrun": "Executes a python3 file, place the file inside the home folder and type 'pyrun [filename]', i.e. 'pyrun hello.py'", "interactive": "Opens an interactive python3 shell, requires 'ipython3' installed", "edit": "Edit or create files, type 'edit [filename]', i.e. 'edit note.txt', requires 'nano' installed"}
+all_commands = {"hello": "A simple command that prints 'Hello World!'", "numguess": "An awesome Guess-The-Number Game", "ls": "Lists all the files and directories inside the home folder", "new": "Creates a new file, use with 'new [filename]', i.e 'new hi.txt', do never use whitespace.", "show": "Prints the contents of a file", "help": "Shows information about a available commands", "exit": "Stops the execution of FakeOS", "pyshell": "Opens an embedded python3 shell", "pyrun": "Executes a python3 file, place the file inside the home folder and type 'pyrun [filename]', i.e. 'pyrun hello.py'", "interactive": "Opens an interactive python3 shell, requires 'ipython' installed", "edit": "Edit or create files, type 'edit [filename]', i.e. 'edit note.txt', requires 'nano' installed"}
 
 def hello():
     print("Hello World!")
@@ -60,11 +60,13 @@ def pyshell():
         subprocess.call("python3")
     print("")
 
-if sys.platform != 'win32':
-    def interactive():
-        print("")
+def interactive():
+    print("")
+    if sys.platform == 'win32':
+        subprocess.call('ipython')
+    else:
         subprocess.call("ipython3")
-        print("")
+    print("")
 
 def edit(filename, editor="nano"):
     try:
