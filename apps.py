62878d062ac214a45b8606b919ab21e42f8eef41
FRostri <frostri.sho755@gmail.com>
2020-11-15 09:42:14 -0500

    Fixing the show command for linux and a form was made so that the user can choose the editor he wants by means of an environment variable.

--- apps.py
+++ apps.py
@@ -2,6 +2,8 @@ import os
 import subprocess
 import sys
 from random import randint
+from dotenv import load_dotenv
+load_dotenv()
 
 all_commands = {"hello": "A simple command that prints 'Hello World!'", "numguess": "An awesome Guess-The-Number Game", "ls": "Lists all the files and directories inside the home folder", "new": "Creates a new file, use with 'new [filename]', i.e 'new hi.txt', do never use whitespace.", "show": "Prints the contents of a file", "help": "Shows information about a available commands", "exit": "Stops the execution of FakeOS", "pyshell": "Opens an embedded python3 shell", "pyrun": "Executes a python3 file, place the file inside the home folder and type 'pyrun [filename]', i.e. 'pyrun hello.py'", "interactive": "Opens an interactive python3 shell, requires 'ipython' installed", "edit": "Edit or create files, type 'edit [filename]', i.e. 'edit note.txt', requires 'nano' installed"}
 
@@ -36,7 +38,7 @@ def show(filename):
     if sys.platform == 'win32':
         os.system('type ' + '.\\home\\' + filename)
     else:
-        subprocess.call(["cat", "/home/$FAKEOSHOME/" + filename])
+        subprocess.call(["cat", "./home/" + filename])
 
 def help(*method):
     if method:
@@ -68,7 +70,8 @@ def interactive():
         subprocess.call("ipython3")
     print("")
 
-def edit(filename, editor="nano"):
+def edit(filename):
+    editor = os.getenv("TEXT_EDITOR")
     try:
         subprocess.call([editor, "/home/$FAKEOSHOME/" + filename])
     except:
