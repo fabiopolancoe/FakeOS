ab85826b988d76735423b6dfdd63026f0df764a8
Sebastian-byte <sebastianmontoya209@gmail.com>
2020-11-14 22:30:45 -0500

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,5 +1,6 @@
 import os
 import subprocess
+import sys
 from random import randint
 
 all_commands = {"hello": "A simple command that prints 'Hello World!'", "numguess": "An awesome Guess-The-Number Game", "ls": "Lists all the files and directories inside the home folder", "new": "Creates a new file, use with 'new [filename]', i.e 'new hi.txt', do never use whitespace.", "show": "Prints the contents of a file", "help": "Shows information about a available commands", "exit": "Stops the execution of FakeOS", "pyshell": "Opens an embedded python3 shell", "pyrun": "Executes a python3 file, place the file inside the home folder and type 'pyrun [filename]', i.e. 'pyrun hello.py'", "interactive": "Opens an interactive python3 shell, requires 'ipython3' installed", "edit": "Edit or create files, type 'edit [filename]', i.e. 'edit note.txt', requires 'nano' installed"}
@@ -40,17 +41,24 @@ def help(*method):
             print(command+": "+all_commands.get(command)+"\n")
 
 def pyrun(filename):
-    subprocess.call(["python3", filename])
+    if sys.platform == 'win32':
+        subprocess.call(["python", filename])
+    else:
+        subprocess.call(["python3", filename])
 
 def pyshell():
     print("")
-    subprocess.call("python3")
+    if sys.platform == 'win32':
+        subprocess.call("python")
+    else:
+        subprocess.call("python3")
     print("")
 
-def interactive():
-    print("")
-    subprocess.call("ipython3")
-    print("")
+if sys.platform != 'win32':
+    def interactive():
+        print("")
+        subprocess.call("ipython3")
+        print("")
 
 def edit(filename):
     try:
