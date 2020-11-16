71379510ad8ac11d9f32e3d9691081416494410f
Fabio Polanco <fabiopolancoe@gmail.com>
2020-11-16 12:44:31 -0600

    Update main.py with Suaj's code :3

--- main.py
+++ main.py
@@ -2,50 +2,65 @@
 
 import signal
 
+# Print the name, if the text2art module is found.
 try:
     from art import text2art
     print(text2art("FakeOS"))
-except:
+except ModuleNotFoundError, NameError:
     pass
 
-print("Welcome to FakeOS!")
+try:
+    import apps
+except ModuleNotFoundError:
+    print("The required module \"apps\" was not found, exiting...")
+    quit()
+    
+from getpass import getuser  # To get the current PC user.
+
+
+print("Welcome to FakeOS!")  # Welcome message, printed always.
 
 
 def signal_handler(signal, frame):
     print("\nBye ;D!")
     quit()
 
-
 def handle_command(data):
+    '''Handles the command the user entered. If the command is not
+    processed for any reason, the functions returns False.'''
     if data:
         method = data[0]
-        del data[0]
+        del data [0]
 
-        if method == "exit":
-            sure = input("Do you really want to logout? [Y/N] ")
-            if sure == "Y" or sure == "y":
-                print("Bye ;D!")
+        if method.lower() == "exit":
+            sure = input("Do you really want to log out? [Y/n]\n")
+            if sure.lower() in ["y", "yes"]:
+                print("Bye! ;D")
                 quit()
-            elif sure == "N" or sure == "n":
-                return
+            elif sure.lower() in ["n", "no", "nope"]:
+                return False
             else:
-                print("You haven't chosen a correct option. Canceling logout...")
-                return
+                print("You did not choose a correct option.")
+                return False
+
         else:
-            import apps
             try:
                 if data:
-                    eval("apps." + method+"(*data)")
+                    eval("apps." + method + "(*data)")
+                    return True
                 else:
-                    eval("apps." + method)()
+                    eval("apps." + method())
+                    return True
             except:
                 print("Couldn't handle that command D:")
-                return
+                return False
+                
     else:
-        pass
+        return False
 
 signal.signal(signal.SIGINT, signal_handler)
 
+# User input
 while True:
-    command = input("FOS > ")
+    command = input(f"{getuser()}@FakeOS: ")
     handle_command(command.split())
