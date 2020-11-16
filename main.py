44ef08839b5f1444fec78aab01f29fbb437c2202
Suaj <72162667+SuajCarrot@users.noreply.github.com>
2020-11-16 22:20:05 +0000

    Espacio innecesario borrado

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+# Main module, originally created by FabioPolancoE
+# Edited by Suaj
+
 import signal
 
 # Print the name, if the text2art module is found.
 try:
     from art import text2art
     print(text2art("FakeOS"))
-except ModuleNotFoundError:
+except ModuleNotFoundError, NameError:
     pass
 
 # The apps module is already included, but just in case...
@@ -32,20 +35,17 @@ def handle_command(data):
     if data:
         method = data[0]
         del data[0]
-
         if method.lower() == "exit":
-            sure = input("Do you really want to log out? [Y/n] ")
+            sure = input("Do you really want to log out? [Y/n]\n")
             if sure.lower() in ["y", "yes"]:
                 print("Bye! ;D")
                 quit()
 
             elif sure.lower() in ["n", "no", "nope"]:
                 return False
-
             else:
                 print("You did not choose a correct option.")
                 return False
-
         else:
             try:
                 if data:
@@ -59,10 +59,10 @@ def handle_command(data):
                 return False      
     else:
         return False
-
 signal.signal(signal.SIGINT, signal_handler)
 
 # User input
 while True:
     command = input(f"[{getuser()}@FakeOS]$ ")
     handle_command(command.split())
+
