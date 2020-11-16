9a400aa3a4ba922074570a81f0e8be2f060d495e
Suaj <72162667+SuajCarrot@users.noreply.github.com>
2020-11-16 23:11:57 +0000

    Código mejorado
    Funciones optimizadas, un poco de documentación añadida y errores de indentación corregidos.

--- apps.py
+++ apps.py
@@ -1,88 +1,103 @@
+#!/usr/bin/env/ python3
+
+# Apps module originally created by Sebastian-Byte, FabioPolancoE and FRostri
+# Edited by Suaj
+
 import os
-import subprocess
 import sys
 import setup
+import subprocess
 from random import randint
 from dotenv import load_dotenv
+
+
 load_dotenv()
 
-all_commands = {"hello": "A simple command that prints 'Hello World!'", "numguess": "An awesome Guess-The-Number Game", "ls": "Lists all the files and directories inside the home folder", "new": "Creates a new file, use with 'new [filename]', i.e 'new hi.txt', do never use whitespace.", "show": "Prints the contents of a file", "help": "Shows information about a available commands", "exit": "Stops the execution of FakeOS", "pyshell": "Opens an embedded python3 shell", "pyrun": "Executes a python3 file, place the file inside the home folder and type 'pyrun [filename]', i.e. 'pyrun hello.py'", "interactive": "Opens an interactive python3 shell, requires 'ipython' installed", "edit": "Edit or create files, type 'edit [filename]', i.e. 'edit note.txt', requires 'nano' installed"}
+# Each command's description
+all_commads = {
+        "hello": "Prints \"Hello, World!\"",
+        "numguess": "An awesome Guess-The-Number game",
+        "ls": "Lists the files and directories inside of the home folder",
+        "new": "Creates a new file by specifying its name",
+        "show": "Prints the contents of a file",
+        "pyshell": "Opens an embedded Python 3 shell",
+        "pyrun": "Executes a Python 3 file",
+        "interactive": "Opens an interactive Python 3 shell. Requires the\
+\"ipython\" module",
+        "edit": "Edit or create files, requires \"nano\" installed",
+        "help": "Shows information about all available commands",
+        "exit": "Stops the execution of FakeOS"
+        }
 
+# Definition of the commands
 def hello():
-    print("Hello World!")
+    print("Hello, World!")
 
 def numguess():
     number = randint(1, 10)
-    guess = int(input("I'm thinking a number from 1 to 10, try to guess it > "))
-
-    if guess == number:
-        print("¡Wow, you did it! :D")
-    else:
-        print("Bad luck, you failed D:")
+    while True:
+        guess = int(input("I'm thinking of a number between 1 and 10, try\
+to guess it...\n")
+        if guess == number:
+            print("Wow, you did it! :D")
+            break
+        else:
+        print("Oops! that isn't the number I'm thinking of D:")
 
 def ls():
     home = os.listdir("./home")
-
     if home:
-        for element in home:
-            print(element+"\n")
-    else:
-        print("Home is empty, fill it with anything you want :D")
+        for i in home:
+            print(i)
+    elif not home or len(home) == 0:
+        print("The home folder is empty.")
 
 def new(filename):
-    if sys.platform == 'win32':
-        os.system("type nul > " + ".\\home\\" + filename)
+    if sys.platform.startswith("win32"):
+        os.system(f"type nul > ./home/{filename}")
     else:
-        subprocess.call(["touch", "./home/" + filename])
+        subprocess.call(["cat", f"./home/{filename}"])
 
 def show(filename):
-    if sys.platform == 'win32':
-        os.system('type ' + '.\\home\\' + filename)
-    else:
-        subprocess.call(["cat", "./home/" + filename])
+    with open(f"./home/{filename}", "r") as f:
+        fcontent = f.readlines()
+    for i in fcontent:
+        print(i, end='')  # Each line already has the \n char at the end
 
-def help(*method):
+def help(method=False):
     if method:
-        print(all_commands.get(method[0]))
+        print(all_commands[method])
     else:
-        print("Running 'help' will print data about all commands, if you want info about only one command, type 'help [command]', i.e. 'help hello'\n")
-        for command in all_commands.keys():
-            print(command+": "+all_commands.get(command)+"\n")
+        print("All available commands:\n")
+        for i in all_commands.keys():
+            print(f"{i}: {all_commands[i]}\n")
 
 def pyrun(filename):
-    if sys.platform == 'win32':
-        os.system("python " + filename)
-    else:
-        subprocess.call(["python3", filename])
+    os.system(f"python {filename}")  # Same command for all platforms
 
 def pyshell():
     print("")
-    if sys.platform == 'win32':
-        os.system("python")
-    else:
-        subprocess.call("python3")
+    os.system("python")
     print("")
 
 def interactive():
     print("")
-    if sys.platform == 'win32':
-        os.system('ipython')
-    else:
-        subprocess.call("ipython3")
+    os.system("ipython")
     print("")
 
 def edit(filename):
     try:
-    	editor = os.getenv("EDITOR")
-		if sys.platform == 'win32':
-	        os.system(editor + ' ' + filename)
-	    else:
-	        subprocess.call([editor, filename])
-	except:
-		if sys.platform == 'win32':
-	        os.system("start notepad.exe " + filename)
-	    else:
-		    subprocess.call(["nano", "./home/" + filename])
+        editor = os.getenv("EDITOR")
+        if sys.platform.startswith("win32"):
+            os.system(f"{editor} {filename}")
+        else:
+            subprocess.call([editor, filename])
+    except:  # There should be an specific exception here
+        if sys.platform.startswith("win32"):
+            os.system(f"start notepad.exe {filename}")
+        else:
+            subprocess.call(["nano", f"./home/{filename}"])
 
 def install():
     setup.start()
+
