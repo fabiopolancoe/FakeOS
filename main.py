5fe174e6dd4dc114d5be3fc1f99035d16f905d13
Sebastian-byte <sebastianmontoya209@gmail.com>
2020-12-16 16:59:15 -0500

    Revert "IDK"
    This reverts commit fc4c571a8f43dda25b92b55a16a294580791ecd4.
    
    Revert Commit

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
 
 # Main module, originally created by FabioPolancoE
 # Edited by Suaj
@@ -9,10 +9,10 @@ import sys
 
 # Print the name, if the text2art module is found.
 try:
-  from art import text2art
-  print(text2art("FakeOS"))
+    from art import text2art
+    print(text2art("FakeOS"))
 except ModuleNotFoundError:
-  pass
+    pass
 
 from getpass import getuser  # To get the current PC user.
 
@@ -20,42 +20,42 @@ print("Welcome to FakeOS!")  # Welcome message, printed always.
 
 
 def signal_handler(signal, frame):
-  print("\nBye ;D!")
-  quit()
+    print("\nBye ;D!")
+    quit()
 
 
 def handle_command(data):
-  '''Handles the command the user entered. If the command is not
-  processed for any reason, the function returns False.'''
-  if data:
-    method = data[0]
-    del data[0]
-    if method.lower() == "exit":
-      sure = input("Do you really want to logout? [Y/n]: ")
-      if sure.lower() in ["y", "yes"]:
-        print("Bye! ;D")
-        sys.exit()
-      
-      elif sure.lower() in ["n", "no", "nope"]:
-        return False
-      else:
-        print("You did not choose a correct option.")
-        return False
-    else:
-      import apps
-      app = apps.func()
-      try:
-        if data:
-          eval("app." + method + "(*data)")
-          return True
+    '''Handles the command the user entered. If the command is not
+    processed for any reason, the function returns False.'''
+    if data:
+        method = data[0]
+        del data[0]
+        if method.lower() == "exit":
+            sure = input("Do you really want to logout? [Y/n]: ")
+            if sure.lower() in ["y", "yes"]:
+                print("Bye! ;D")
+                sys.exit()
+
+            elif sure.lower() in ["n", "no", "nope"]:
+                return False
+            else:
+                print("You did not choose a correct option.")
+                return False
         else:
-          eval("app." + method + "()")
-          return True
-      except:
-        print("Couldn't handle that command D:")
+            import apps
+            app = apps.func()
+            try:
+                if data:
+                    eval("app." + method + "(*data)")
+                    return True
+                else:
+                    eval("app." + method + "()")
+                    return True
+            except:
+                print("Couldn't handle that command D:")
+                return False
+    else:
         return False
-  else:
-    return False
 signal.signal(signal.SIGINT, signal_handler)
 
 # User input
